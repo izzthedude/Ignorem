d8558585f914c783276a0b45457751c313446eef
Izzat Z. <izzat.zainir11@gmail.com>
2023-09-17 01:23:22 +0800

    Hotfix: Move gi.require_version to ignorem init

--- ignorem/app.py
+++ ignorem/app.py
@@ -18,10 +18,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import sys
-import gi
 
-gi.require_version("Gtk", "4.0")
-gi.require_version("Adw", "1")
 from gi.repository import Gio, Gtk, Adw
 
 from ignorem.ui.window import MainWindow
