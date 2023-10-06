ab45160c66542ad016cc03e6bc10656c19acff90
Izzat Z. <izzat.zainir11@gmail.com>
2023-10-07 04:38:51 +0800

    Adjust AppController to use gitignoreio
    * Also create Singleton class

--- ignorem/base/types.py
+++ ignorem/base/types.py
@@ -1,7 +1,31 @@
+from __future__ import annotations
+
 import os
-from typing import Mapping, Sequence
+from typing import Any, Mapping, Self, Sequence, Type
 
 JSONLike = (
     Mapping[str, str | int | bool] | Mapping[str, "JSONLike"] | Sequence["JSONLike"]
 )
 PathLike = os.PathLike | str
+
+
+class SingletonError(Exception):
+    def __init__(self) -> None:
+        super().__init__(
+            "Singletons can only be accessed through the instance() class method."
+        )
+
+
+class Singleton:
+    _instance: dict[Type[Self], Self] = {}
+
+    def __new__(cls: Type[Self], *args: Any, **kwargs: Any) -> Self:
+        if cls in cls._instance:
+            raise SingletonError
+        return super().__new__(cls)
+
+    @classmethod
+    def instance(cls: Type[Self]) -> Self | Singleton:
+        if cls not in cls._instance:
+            cls._instance[cls] = cls()
+        return cls._instance[cls]
--- ignorem/controller.py
+++ ignorem/controller.py
@@ -1,56 +1,45 @@
-from ignorem import gitignore
+from __future__ import annotations
+
+from ignorem.base.types import Singleton
 from ignorem.constants import Data
-from ignorem.gitignore import TemplateData
+from ignorem.gitignoreio.apis import GitIgnoreAPI, GitIgnoreListAPI
+from ignorem.gitignoreio.models import TemplateModel
 from ignorem.utils import files
 
 
-class AppController:
-    _instance: "AppController" = None
+class AppController(Singleton):
+    def __init__(self) -> None:
+        self._templates: list[TemplateModel] = []
+        self._selected_templates: list[TemplateModel] = []
 
-    @classmethod
-    def instance(cls):
-        if not cls._instance:
-            cls._instance = AppController()
-        return AppController._instance
-
-    def __init__(self):
-        if AppController._instance:
-            raise Exception(
-                "An instance of this class already exists. "
-                "Use 'AppController.instance()' to get it."
-            )
-
-        self._templates: list[TemplateData] = []
-        self._selected_templates: list[TemplateData] = []
-
-    def fetch_list(self, force_fetch: bool = False) -> list[TemplateData]:
+    def fetch_list(self, force_fetch: bool = False) -> list[TemplateModel]:
         cache_file = Data.CACHE_FILE
         if not cache_file.exists() or force_fetch:
             files.write_json(
-                [template.as_dict() for template in gitignore.list_json()],
+                [template.to_dict() for template in GitIgnoreListAPI.get("json")],
                 Data.CACHE_FILE,
             )
 
         self._templates = [
-            TemplateData(**template) for template in files.read_json(Data.CACHE_FILE)
+            TemplateModel(**template) for template in files.read_json(Data.CACHE_FILE)
         ]
         return self._templates
 
-    def templates(self) -> list[TemplateData]:
+    def templates(self) -> list[TemplateModel]:
         return self._templates
 
-    def selected_templates(self) -> list[TemplateData]:
+    def selected_templates(self) -> list[TemplateModel]:
         return self._selected_templates
 
-    def add_selected_template(self, template: TemplateData):
+    def add_selected_template(self, template: TemplateModel) -> None:
         self._selected_templates.append(template)
 
-    def remove_selected_template(self, template: TemplateData):
+    def remove_selected_template(self, template: TemplateModel) -> None:
         self._selected_templates.remove(template)
 
     def request_template(self) -> str:
         keys = [template.key for template in self._selected_templates]
-        return gitignore.get_template(keys)
+        return GitIgnoreAPI.get(*keys)
 
-    def reset(self):
+    def reset(self) -> None:
         self._selected_templates.clear()
