5c62a37bca4d2ed1002c685a4229aa665a494de1
Izzat Z. <izzat.zainir11@gmail.com>
2023-10-07 15:15:28 +0800

    Unignore typing for utils.ui and apply fixes

--- ignorem/utils/ui.py
+++ ignorem/utils/ui.py
@@ -1,14 +1,17 @@
-from typing import Any, Callable
+from typing import Any, Callable, Optional, ParamSpec, Type, TypeVar
 
 from gi.repository import Adw, GObject, Gio
+
+P = ParamSpec("P")
+T = TypeVar("T")
 
 
 def create_action(
     source: Adw.Application | Adw.ApplicationWindow,
     action_name: str,
-    callback,
-    shortcuts: list | None = None,
-):
+    callback: Callable[P, T],
+    shortcuts: Optional[list[str]] = None,
+) -> None:
     action = Gio.SimpleAction.new(action_name, None)
     action.connect("activate", callback)
     source.add_action(action)
@@ -16,14 +19,19 @@ def create_action(
     if shortcuts:
         app = source
         origin = "app"
-        if isinstance(app, Adw.ApplicationWindow):
+
+        if isinstance(source, Adw.ApplicationWindow):
             app = source.get_application()
             origin = "win"
 
         app.set_accels_for_action(f"{origin}.{action_name}", shortcuts)
 
 
-def create_signal(source: GObject.Object, signal_name: str, param_types: tuple = ()):
+def create_signal(
+    source: GObject.Object,
+    signal_name: str,
+    param_types: tuple[Type[Any]] | tuple[()] = (),
+) -> None:
     GObject.signal_new(
         signal_name,  # Signal name
         source,  # A Python GObject instance or type that the signal is associated with
@@ -35,17 +43,17 @@ def create_signal(source: GObject.Object, signal_name: str, param_types: tuple =
 
 def run_async(
     source: GObject.Object,
-    func: Callable,
-    callback: Callable | None = None,
-    func_args: tuple = (),
-    callback_args: tuple = (),
+    func: Callable[P, T],
+    callback: Optional[Callable[P, T]] = None,
+    func_args: tuple[Any] | tuple[()] = (),
+    callback_args: tuple[Any] | tuple[()] = (),
 ) -> Gio.Task:
     def _func(
         task: Gio.Task,
         source: GObject.Object,
         data: Any,
-        cancellable: Gio.Cancellable | None,
-    ):
+        cancellable: Optional[Gio.Cancellable],
+    ) -> None:
         func_name = func.__name__
 
         try:
@@ -55,12 +63,12 @@ def run_async(
         except Exception as err:
             print(f"An error has occurred while running task '{func_name}':", err)
 
-    def _callback(task: Gio.Task, status: GObject.ParamSpecBoolean):
-        callback_name = callback.__name__
+    def _callback(callback_: Callable[P, T]) -> None:
+        callback_name = callback_.__name__
 
         try:
             print(f"Running callback: {callback_name}")
-            callback(*callback_args)
+            callback_(*callback_args)
             print(f"Finished callback: {callback_name}")
         except Exception as err:
             print(f"An error has occurred while running task '{callback_name}':", err)
@@ -69,8 +77,9 @@ def run_async(
     task.set_check_cancellable(True)
     task.set_return_on_cancel(True)
     task.return_error_if_cancelled()
+
     if callback:
-        task.connect("notify::completed", _callback)
+        task.connect("notify::completed", lambda *_: _callback(callback))
 
     task.run_in_thread(_func)
     return task
